49a177c98d48f0347b95a0f1cd3d4ae5d7822405
Marcos <marcos_moraes26@outlook.com>
2021-06-17 12:06:26 -0300

    feat: validação de email

--- sistema/Login.php
+++ sistema/Login.php
@@ -1,15 +1,23 @@
 <?php
 
-// Classe para 
+require_once('../config/security.php');
 
-class login{
+$email = $_POST['user_mail'];
 
-
-
+// Aqui ocorre a verificação do email !!
+if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+    echo "This email: ".$email." STATUS : VALID";
 }
 
+// aqui eu gero uma hash criptografada, pego a hora que foi gerada essa hash e o ip do usuário
+$hashMD5 = crypt(rand(0,20).time().$_SERVER['REMOTE_ADDR'],'$2a$07$'. SECURITY_HASH);
 
-$a ="ab";
+
+$takeIp =$_SERVER['REMOTE_ADDR'];
+$takeDate = getdate();
+echo $takeDate;
+
+
 
 
 
